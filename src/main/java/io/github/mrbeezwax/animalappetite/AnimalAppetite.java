41a6107a25b8c003e13010d8388ac379edec4d56
Juju Liao <liaojulian@gmail.com>
2019-01-01 18:13:48 -0800

    Moved scoreboard to main

--- src/main/java/io/github/mrbeezwax/animalappetite/AnimalAppetite.java
+++ src/main/java/io/github/mrbeezwax/animalappetite/AnimalAppetite.java
@@ -1,14 +1,21 @@
 package io.github.mrbeezwax.animalappetite;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
 
 public final class AnimalAppetite extends JavaPlugin {
+    public Scoreboard scoreboard = getServer().getScoreboardManager().getMainScoreboard();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("Plugin Enabled");
         new BreedEventListener(this);
+
+        // Scoreboard Set-Up
+        scoreboard.registerNewTeam("infertile");
+        scoreboard.getTeam("infertile").setColor(ChatColor.DARK_RED);
     }
 
     @Override
--- src/main/java/io/github/mrbeezwax/animalappetite/BreedEventListener.java
+++ src/main/java/io/github/mrbeezwax/animalappetite/BreedEventListener.java
@@ -1,11 +1,9 @@
 package io.github.mrbeezwax.animalappetite;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityBreedEvent;
-import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
 import java.util.Timer;
@@ -19,14 +17,7 @@ public class BreedEventListener implements Listener {
     @EventHandler
     public void onEntityBreed(EntityBreedEvent event) {
         System.out.println("Breed Event");
-        Bukkit.broadcastMessage("Breed Event");
-        Bukkit.broadcastMessage("Bred with: " + event.getBredWith().toString());
-        Bukkit.broadcastMessage("Bred by: " + event.getBreeder().getName());
-        Bukkit.broadcastMessage("Bred to: " + event.getEntity().getName());
-
-        Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
-        Team infertile = scoreboard.registerNewTeam("infertile");
-        infertile.setColor(ChatColor.DARK_RED);
+        Team infertile = Bukkit.getScoreboardManager().getMainScoreboard().getTeam("infertile");
         infertile.addEntry(event.getMother().getUniqueId().toString());
         infertile.addEntry(event.getFather().getUniqueId().toString());
         event.getMother().setGlowing(true);
@@ -39,8 +30,7 @@ public class BreedEventListener implements Listener {
                 event.getMother().setGlowing(false);
                 infertile.removeEntry(event.getMother().getUniqueId().toString());
                 infertile.removeEntry(event.getFather().getUniqueId().toString());
-                infertile.unregister();
             }
-        }, 3000);
+        }, 300000);
     }
 }

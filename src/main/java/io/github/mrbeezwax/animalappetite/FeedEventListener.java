2d6f250075bf4f7cda8f2d9882d9e4d50f93d757
Juju Liao <liaojulian@gmail.com>
2019-01-02 20:17:37 -0800

    Incompleted Feed Mechanic
    Need to add cooldown to hashmap removal

--- src/main/java/io/github/mrbeezwax/animalappetite/FeedEventListener.java
+++ src/main/java/io/github/mrbeezwax/animalappetite/FeedEventListener.java
@@ -3,12 +3,18 @@ package io.github.mrbeezwax.animalappetite;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Animals;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
+import java.util.HashMap;
+
 public class FeedEventListener implements Listener {
+    private HashMap<Entity, Integer> fedEntities = new HashMap<>();
+    private final int BREEDING_LIMIT = 3;
+
     public FeedEventListener(AnimalAppetite plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
@@ -16,9 +22,20 @@ public class FeedEventListener implements Listener {
     @EventHandler
     public void onEntityFeed(PlayerInteractEntityEvent event) {
         Player p = event.getPlayer();
+        Entity e = event.getRightClicked();
         if (event.getRightClicked() instanceof Animals) {
             if (p.getInventory().getItemInMainHand().getType() == Material.CARROT) {
+                if (fedEntities.containsKey(e)) {
+                    fedEntities.put(e, fedEntities.get(e) + 1);
+                    if (fedEntities.get(event.getRightClicked()) == BREEDING_LIMIT) {
+                        fedEntities.remove(e);
+                        return;
+                    }
+                } else {
+                    fedEntities.put(e, 1);
+                }
                 event.setCancelled(true);
+                p.getInventory().getItemInMainHand().setAmount(p.getInventory().getItemInMainHand().getAmount() - 1);
                 p.updateInventory();
                 Bukkit.broadcastMessage("Feed Event");
             }
